80e4f71c148ed5bb4bda119b2fc37c44d2bf6163
Daniel <daniel@Daniels-MacBook-Pro.local>
2020-01-02 22:29:16 +0300

    Added business service

--- src/main/java/io/mbeyah/springbootstarter/topic/TopicController.java
+++ src/main/java/io/mbeyah/springbootstarter/topic/TopicController.java
@@ -3,19 +3,18 @@ package io.mbeyah.springbootstarter.topic;
 import java.util.List;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("Spring", "Spring Framework", "Spring framework description"),
-				new Topic("Spring", "Spring Framework", "Spring framework description"),
-				new Topic("Spring", "Spring Framework", "Spring framework description"),
-				new Topic("Spring", "Spring Framework", "Spring framework description")
-				);
+		return topicService.getAllTopics();
 	}
 }
---
+++ src/main/java/io/mbeyah/springbootstarter/topic/TopicService.java
@@ -0,0 +1,20 @@
+package io.mbeyah.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("Spring", "Spring Framework", "Spring framework description"),
+			new Topic("Java", "Spring Framework", "Spring framework description"),
+			new Topic("Javascript", "Spring Framework", "Spring framework description")
+			);
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+}
